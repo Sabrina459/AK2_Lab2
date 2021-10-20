9b21a992da692ed33112640f7aff65dcb91487db
Sabrina459 <sabrinas.ua82@gmail.com>
2021-10-20 12:09:15 +0300

    Lab2 commit

--- calculator.h
+++ calculator.h
@@ -1,11 +1,9 @@
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
 
-class Calculator
-{
-	public:
-		int Add (double, double);
-		int Sub (double, double);
-};
+
+int Add (double, double);
+int Sub (double, double);
+
 
 #endif//CALCULATOR_H
